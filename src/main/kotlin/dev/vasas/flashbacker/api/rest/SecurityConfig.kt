a7d51eb89d699d4caafb620a1101c60d40bf0790
Szabolcs Vasas <vasas@apache.org>
2019-12-28 11:41:47 +0000

    Test Spring profile is introduced
    * Automated tests are now running with the 'test' profile activated.
    * The custom security configuration and the external DynamoDB configuration beans are disabled for the test profile.
    * Specifying prod profile is not necessary anymore for production usage.

--- src/main/kotlin/dev/vasas/flashbacker/api/rest/SecurityConfig.kt
+++ src/main/kotlin/dev/vasas/flashbacker/api/rest/SecurityConfig.kt
@@ -6,8 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 
+/**
+ * This bean overrides the auto configuration in
+ * [org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerJwtConfiguration.OAuth2WebSecurityConfigurerAdapter].
+ *
+ * It enables CORS and makes sure that we do not manage sessions for the clients.
+ *
+ * This bean must not be created when running automated tests since they use mock security configuration.
+ */
 @Configuration
-@Profile("prod")
+@Profile("!test")
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
--- src/main/kotlin/dev/vasas/flashbacker/persistence/dynamodb/DynamoDbConfiguration.kt
+++ src/main/kotlin/dev/vasas/flashbacker/persistence/dynamodb/DynamoDbConfiguration.kt
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("prod")
+@Profile("!test")
 class DynamoDbConfiguration(
         @Value("\${flashbacker.dynamodb.endpoint}")
         private val endpoint: String,
